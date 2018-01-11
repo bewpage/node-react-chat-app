e3dd8fda69dfea3d35a372dd0388828a25d01a51
bewpage <b.jedlinski@bewpage.com>
2018-01-11 10:56:59 +0100

    removed console.logs

--- client/src/components/MessageList.jsx
+++ client/src/components/MessageList.jsx
@@ -9,12 +9,12 @@ class MessageList extends Component {
 
 
     render() {
-        console.log('test sprawdzic co to jest props', this.props);
+        // console.log('test sprawdzic co to jest props', this.props);
         return (
             <div className='sc-message-list'>
                 <ul>
                     {this.props.data.received.map((message, i) => {
-                        console.log('test sprawdzic co to jest', message);
+                        // console.log('test sprawdzic co to jest', message);
                         return(
                             <Message key={i} message={message.text} from={message.from}/>
                         )
